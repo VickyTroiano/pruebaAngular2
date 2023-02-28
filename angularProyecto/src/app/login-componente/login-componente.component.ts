2c08daa5d78e1e2b141c5dae458982e04c6fdf42
Victoria Troiano <92123389+VickyTroiano@users.noreply.github.com>
2023-02-28 14:36:35 -0300

    Creacion del proyecto angularProyecto y componente loginComponente

---
+++ angularProyecto/src/app/login-componente/login-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-componente',
+  templateUrl: './login-componente.component.html',
+  styleUrls: ['./login-componente.component.css']
+})
+export class LoginComponenteComponent {
+
+}
