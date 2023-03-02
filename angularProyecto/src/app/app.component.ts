5a3e26d788110b416f33b48638448983243e4ec1
Victoria Troiano <92123389+VickyTroiano@users.noreply.github.com>
2023-03-02 13:18:48 -0300

    Prueba 2: binding Property

--- angularProyecto/src/app/app.component.ts
+++ angularProyecto/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularProyecto';
   urlImg = '../assets/img/montania-1280x795.png'; //Binding Property ejemplo 1. Del componente al DOM
+  bienvenida = 'Bienvenidos a mi pagina';
 }
